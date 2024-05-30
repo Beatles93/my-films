d33eb230f0bb95cf51306a683ce6fce23cc08a72
Zhuk Evheny <113713601+Beatles93@users.noreply.github.com>
2024-05-30 11:32:04 +0300

    fix search input

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,21 @@
+import { useLocation } from "react-router-dom";
 import "./styles.css";
 import Navbar from "../Navbar/Navbar";
 
 const Header = ({ setQuery }) => {
+  const location = useLocation();
+
   const handleInputChange = (e) => {
     setQuery(e.target.value);
   };
+
+  const isFilmsPage = location.pathname === "/films";
 
   return (
     <div className="header">
       <h2 className="logo">My films</h2>
       <div className="center-container">
+          {isFilmsPage && (
         <div className="search-container">
           <input
             type="text"
@@ -17,24 +23,8 @@ const Header = ({ setQuery }) => {
             placeholder="Search for movies..."
             onChange={handleInputChange}
           />
-          <button className="search-button">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="feather feather-search"
-            >
-              <circle cx="11" cy="11" r="8"></circle>
-              <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-            </svg>
-          </button>
-        </div>
+          </div>
+          )}
       </div>
       <Navbar />
     </div>
