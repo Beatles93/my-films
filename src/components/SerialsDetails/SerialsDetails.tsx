dd179e4bc786e63c26bff65c0b6a74125acbba34
Zhuk Evheny <113713601+Beatles93@users.noreply.github.com>
2024-06-13 11:47:17 +0300

    feat update fetch

--- src/components/SerialsDetails/SerialsDetails.tsx
+++ src/components/SerialsDetails/SerialsDetails.tsx
@@ -1,88 +1,96 @@
-import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import loaderSvg from "../../assets/loader.svg";
-import StarRating from "../StarRating/StarRating";
-import {
-  LoaderContainer,
-  Loader,
-  Container,
-  Poster,
-  Title,
-  Text,
-  Votes,
-  VoteCount,
-  BackButton,
-} from "./styled-components";
+ import { useEffect, useState } from "react";
+ import { useParams, useNavigate } from "react-router-dom";
+ import loaderSvg from "../../assets/loader.svg";
+ import StarRating from "../StarRating/StarRating";
+ import {
+   LoaderContainer,
+   Loader,
+   Container,
+   Poster,
+   Title,
+   Text,
+   Votes,
+   VoteCount,
+   BackButton,
+ } from "./styled-components";
 
-interface Series {
-  poster_path: string;
-  original_name: string;
-  first_air_date: string;
-  overview: string;
-  vote_average: number;
-  vote_count: number;
-}
+ interface Series {
+   poster_path: string;
+   original_name: string;
+   first_air_date: string;
+   overview: string;
+   vote_average: number;
+   vote_count: number;
+ }
 
-const SerialsDetails: React.FC = () => {
-  const { id } = useParams<{ id: string }>();
-  const navigate = useNavigate();
-  const [serialDetails, setSerialDetails] = useState<Series | null>(null);
-  const [loading, setLoading] = useState(true);
+ const SerialsDetails: React.FC = () => {
+   const { id } = useParams<{ id: string }>();
+   const navigate = useNavigate();
+   const [serialDetails, setSerialDetails] = useState<Series | null>(null);
+   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    if (!id) {
-      console.error("No ID provided");
-      setLoading(false);
-      return;
-    }
+   useEffect(() => {
+     if (!id) {
+       console.error("No ID provided");
+       setLoading(false);
+       return;
+     }
 
-    fetch(
-      `https://api.themoviedb.org/3/tv/${id}?api_key=a1a8715f3d20282103a4496ee0f9ac4d`
-    )
-      .then((res) => res.json())
-      .then((data: Series) => {
-        setSerialDetails(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching serial details:", error);
-        setLoading(false);
-      });
-  }, [id]);
+     const options = {
+       method: "GET",
+       headers: {
+         accept: "application/json",
+       },
+     };
 
-  if (loading) {
-    return (
-      <LoaderContainer>
-        <Loader src={loaderSvg} alt="Loading..." />
-      </LoaderContainer>
-    );
-  }
+     fetch(
+       `https://api.themoviedb.org/3/tv/${id}?api_key=940420b28116a0814ea5530e8f40f139&language=en-US`,
+       options
+     )
+       .then((res) => res.json())
+       .then((data: Series) => {
+         setSerialDetails(data);
+         setLoading(false);
+       })
+       .catch((error) => {
+         console.error("Error fetching serial details:", error);
+         setLoading(false);
+       });
+   }, [id]);
 
-  if (!serialDetails) {
-    return <div>Error loading serial details</div>;
-  }
+   if (loading) {
+     return (
+       <LoaderContainer>
+         <Loader src={loaderSvg} alt="Loading..." />
+       </LoaderContainer>
+     );
+   }
 
-  return (
-    <Container>
-      <Poster
-        src={`https://image.tmdb.org/t/p/w500${serialDetails.poster_path}`}
-        alt={serialDetails.original_name}
-      />
-      <Title>{serialDetails.original_name}</Title>
-      <Text>First Air Date: {serialDetails.first_air_date}</Text>
-      <Text>{serialDetails.overview}</Text>
-      <Votes>
-        <VoteCount>
-          <StarRating rating={serialDetails.vote_average / 2} />
-          {serialDetails.vote_average} / 10
-        </VoteCount>
-        <VoteCount>
-          <i className="fas fa-star"></i> {serialDetails.vote_count} 
-        </VoteCount>
-      </Votes>
-      <BackButton onClick={() => navigate(-1)}>Back to Serials</BackButton>
-    </Container>
-  );
-};
+   if (!serialDetails) {
+     return <div>Error loading serial details</div>;
+   }
 
-export default SerialsDetails;
+   return (
+     <Container>
+       <Poster
+         src={`https://image.tmdb.org/t/p/w500${serialDetails.poster_path}`}
+         alt={serialDetails.original_name}
+       />
+       <Title>{serialDetails.original_name}</Title>
+       <Text>First Air Date: {serialDetails.first_air_date}</Text>
+       <Text>{serialDetails.overview}</Text>
+       <Votes>
+         <VoteCount>
+           <StarRating rating={serialDetails.vote_average / 2} />
+           {serialDetails.vote_average} / 10
+         </VoteCount>
+         <VoteCount>
+           <i className="fas fa-star"></i> {serialDetails.vote_count}
+         </VoteCount>
+       </Votes>
+       <BackButton onClick={() => navigate(-1)}>Back to Serials</BackButton>
+     </Container>
+   );
+ };
+
+ export default SerialsDetails;
